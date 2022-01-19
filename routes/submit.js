e57f825071a9d2486a876f61445bda225450a3cb
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-01-19 18:10:22 +0600

    submit route initiated

---
+++ routes/submit.js
@@ -0,0 +1,20 @@
+/*
+ * Title: Submit Route
+ * Description: Submit Route for app
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mmesba
+ * Date: 19/01/2022
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ 
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ 
