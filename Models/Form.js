ea038ace66916fec9f006fafe70cf7e44e37f3b7
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-01-19 18:45:02 +0600

    Form Model created using form schema

--- Models/Form.js
+++ Models/Form.js
@@ -7,14 +7,33 @@
  */
  
 // Dependencies.
- 
- 
-// App object or Module scaffolding.
- 
+ const mongoose = require('mongoose');
+  
 // main functions or objects.
+//  Creating Form Schema
+const formSchema = mongoose.Schema({
+    name: {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required: true
+    },
+    subject : {
+        type : String,
+        required : true
+    },
+    text : {
+        type : String,
+        required : true
+    }
+})
  
  
- 
- 
+// Creating Model using  form schema
+const FormModel = mongoose.model('Contact', formSchema);
+
+
 // export the module.
- 
+ module.exports = FormModel;
