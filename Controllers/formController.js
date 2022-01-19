37c27fbb6fe0b961a529ba5ed90c43a69264e56b
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-01-19 18:59:56 +0600

    Save data function added

--- Controllers/formController.js
+++ Controllers/formController.js
@@ -7,13 +7,26 @@
  */
  
 // Dependencies.
- 
-// App object or Module scaffolding.
- 
+ const FormModel = require('../Models/Form');
+  
 // main functions or objects.
- 
+ const submit = (req, res)=>{
+   const formData = new FormModel({
+       name : req.body.name,
+       email : req.body.email,
+       subject : req.body.subject,
+       text : req.body.txt 
+   })
+   formData.save((err)=>{
+       if (err) {
+           res.status(500).send('error  '+err.message) 
+         } else {
+            res.render({data: req.body})
+        }
+   })
+ }
  
  
  
 // export the module.
- 
+ module.exports = submit;
