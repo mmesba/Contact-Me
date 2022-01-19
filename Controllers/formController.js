cf14eb6812d8571fbb15d49e8c22083a3e6c9424
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-01-19 18:49:39 +0600

    Form Controller file initiated

---
+++ Controllers/formController.js
@@ -0,0 +1,19 @@
+/*
+ * Title: Form Controller File
+ * Description: Form Controller File for api
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mmesba
+ * Date: 19/01/2022
+ */
+ 
+// Dependencies.
+ 
+// App object or Module scaffolding.
+ 
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ 
