225d60f1388e3f89b71076c0b869d8fe0d568a03
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-01-19 18:36:14 +0600

    Form model file initiated

---
+++ Models/Form.js
@@ -0,0 +1,20 @@
+/*
+ * Title: Form Model
+ * Description: Form Model to store user data
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mmesba
+ * Date: 19/01/2022
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ 
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ 
