a1bd8abb1aaaf008544768add3395d52cca8954e
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-01-23 00:04:09 +0600

    awake module initiated

---
+++ awake.js
@@ -0,0 +1,20 @@
+/*
+ * Title: Awake Idling Module
+ * Description: Awake Idling Servers by frequent requesting.
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mmesba
+ * Date: 23/01/2022
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ const awake = {};
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ module.exports = awake;
