f75afa39f439fc36a505db928a97029167ed0829
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-01-24 22:50:36 +0600

    Jest installed and testing file initiated

---
+++ __tests__/lib.test.js
@@ -0,0 +1,20 @@
+/*
+ * Title: Testing File
+ * Description: Project's test cases
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mmesba
+ * Date: 24/01/2022
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ 
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ 
